40e2650bf966011a449ad634ceca3b646b429145
Matagne <yan.matagne@gmail.com>
2013-04-05 02:24:54 +0200

    Add server class for heroku

---
+++ src/main/java/root/StartWebApp.java
@@ -0,0 +1,22 @@
+package root;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+ 
+public class StartWebApp {
+    public static void main(String[] args) throws Exception {
+        String portStr = System.getenv("PORT");
+        int port = (portStr == null) ? 8085 : Integer.parseInt(portStr);
+        Server server = new Server(port);
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
+        // change the name of the war as needed.
+        webapp.setWar("target/blog.war");
+        server.setHandler(webapp);
+ 
+        server.start();
+        server.join();
+    }
+ 
+ 
+}
